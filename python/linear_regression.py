d0850319198a8ec34bc9a54441344c883868a7fe
ipod1g <kubonkwan99@gmail.com>
2023-10-12 18:54:44 +0800

    ♻️ change cvp to smaller test splits

--- python/linear_regression.py
+++ python/linear_regression.py
@@ -29,8 +29,7 @@ data = data * 10
 cutoff = 5
 idxorig = np.where(np.sum(data < cutoff, axis=(0, 2)))
 
-# datamerge = np.concatenate([data[:, :, 0], data[:, :, 1]], axis=0)
-datamerge = np.concatenate([data[:, :, 0], data[:, :, 1]])
+datamerge = np.concatenate([data[:, :, 0], data[:, :, 1]], axis=0)
 
 dataNorm = normalize(1.0 / datamerge)
 R = np.corrcoef(dataNorm.T)
@@ -65,46 +64,54 @@ for t in range(trainRepeat):
         (np.ones((data.shape[0], 1)), np.zeros((data.shape[0], 1))))
 
     test_size = 0.4
-    cvp = train_test_split(range(Y.shape[0]), test_size=test_size)
+    Y_size = Y.shape[0]
 
-    idxTrain, idxTest = cvp
+    x_train, x_test, y_train, y_test = train_test_split(
+        X, Y, test_size=test_size, random_state=42)
 
-    # X = X.T
+    x_train = x_train.astype(int)
+    x_test = x_test.astype(int)
+    y_train = y_train.astype(int)
+    y_test = y_test.astype(int)
+
+    X = X.T
     lambda_vals = np.logspace(-6, -0.5, len(idx))
 
     Mdl = LogisticRegression(penalty='l1', solver='saga',
                              C=1.0 / lambda_vals[0], max_iter=1000, tol=1e-8)
 
-    Mdl.fit(X[idxTrain, :], Y[idxTrain])
-    labels = Mdl.predict(X[idxTest, :])
-    L = Mdl.score(X[idxTest, :], Y[idxTest])
-    # Mdl.fit(X[:, idxTrain], Y[idxTrain])
-    # labels = Mdl.predict(X[:, idxTest])
+    Mdl.fit(x_train, y_train)
+    # needs to be datamerge columns but its 1 now,,,
+    labels = Mdl.predict(x_test)
+    L = Mdl.score(x_test, y_test)
+
+    # Mdl.fit(X[idxTrain, :], Y[idxTrain])
+    # labels = Mdl.predict(X[idxTest, :])
     # L = Mdl.score(X[:, idxTest], Y[idxTest])
 
-    # importance = np.zeros(data.shape[1])
-    # importance[idx] = np.abs(Mdl.coef_[0])
-    # importance = np.reshape(importance, (590, int(data.shape[1] / 590)))
-    # print(importance)
-    # importance = normalize(importance, axis=0)
-    # # importance = normalize(importance.reshape((590, -1)), axis=0).T
-    # iptorig = np.sum(importance, axis=0)
-    # ipt = normalize(iptorig)
+    importance = np.zeros((1, data.shape[1]))
+    # importance[idx] = np.abs(Mdl.coef_[:,1])
+    importance[0, idx] = np.abs(Mdl.coef_[0])
+    reshaped_importance = np.reshape(
+        importance, (590, int(data.shape[1] / 590)))
+    normalized_importance = normalize(reshaped_importance, norm='l2', axis=0)
+    iptorig = np.nansum(normalized_importance, axis=1)
+    ipt = normalize(iptorig)
 
-    # this one
+    iptglob[t, :] = ipt
+
+    # reference
     # importance = np.zeros(data.shape[1])
     # importance[idx] = np.abs(Mdl.coef_[:,1])
     # importance = np.reshape(590, (-1, 1))
     # importance = normalize(importance, norm='l2', axis=0)
     # iptorig = np.nansum(importance, axis=1)
     # ipt = normalize(iptorig.reshape(-1, 1), axis=0, norm='max')
-
     # iptglob[t, :] = ipt
 
-
+# %%
 ipt = np.mean(iptglob, axis=0)
 
-# %%
 plt.figure(figsize=(8, 3))
 plt.plot(range(35, 330), ipt[:295], linewidth=1.5)
 plt.xlabel('Residue index')
@@ -120,3 +127,5 @@ plt.ylabel('Importance')
 plt.xlim([35, 329])
 plt.ylim([-0.1, 1.1])
 plt.show()
+
+# %%
